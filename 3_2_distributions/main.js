b79963c6ad6fb73e79a2721ccb5910e4301e4613
Ellie Frymire <ellie.frymire@gmail.com>
2024-01-29 11:21:37 -0800

    add demos

--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,15 +1,15 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 },
+  radius = 5;
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
+// these variables allow us to define anything we manipulate in init() but need access to in draw().
 // All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+let svg;
+let xScale;
+let yScale;
+let colorScale;
 
 /* APPLICATION STATE */
 let state = {
@@ -19,7 +19,6 @@ let state = {
 
 /* LOAD DATA */
 d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
   console.log("data", raw_data);
   // save our data to application state
   state.data = raw_data;
@@ -30,20 +29,56 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 // this will be run *one time* when the data finishes loading in
 function init() {
   // + SCALES
+  xScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d => d.ideologyScore2020))
+    .range([margin.left, width - margin.right]);
+  
+  yScale = d3.scaleLinear()
+    .domain(d3.extent(state.data, d => d.envScore2020))
+    .range([height-margin.top, margin.bottom]);
 
+  colorScale = d3.scaleOrdinal()
+    .domain(["R", "D"])
+    .range(["red", "blue"])
 
   // + AXES
-
+  const xAxis = d3.axisBottom(xScale)
+  const yAxis = d3.axisLeft(yScale)
 
   // + UI ELEMENT SETUP
+  const selectElement = d3.select("#dropdown")
+
+  selectElement  
+    .selectAll("option")
+    .data([...Array.from(new Set(state.data.map(d => d.Party))), "All"])
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d)
+
+  selectElement
+    .on("change", (event) => {
+      console.log(event)
+      state.selectedParty = event.target.value;
+      draw();
+    })
 
 
   // + CREATE SVG ELEMENT
-
+  svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
 
   // + CALL AXES
-
-
+  svg
+    .append("g")
+    .call(xAxis)
+    .attr("transform", `translate(0,${height - margin.top})`)
+  
+  svg
+    .append("g")
+    .call(yAxis)
+    .attr("transform", `translate(${margin.left},0)`)
 
   draw(); // calls the draw function
 }
@@ -54,19 +89,47 @@ function draw() {
 
   // + FILTER DATA BASED ON STATE
   const filteredData = state.data
-    // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
+    .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
 
   const dot = svg
     .selectAll("circle")
-    .data(filteredData, d => d.BioID)
+    .data(filteredData, d => `id_${d.Party}_${d.envScore2020}_${d.ideologyScore2020}`)
     .join(
       // + HANDLE ENTER SELECTION
-      enter => enter,
+      enter => enter.append("circle")
+        .attr("r", radius)
+        .attr("cx", 0)
+        .attr("cy", d => yScale(d.envScore2020))
+        .attr("fill", d => colorScale(d.Party))
+        .call(sel => sel
+          .transition()
+          .duration(1000)
+          .attr("cx", d => xScale(d.ideologyScore2020))
+        ),
 
       // + HANDLE UPDATE SELECTION
-      update => update,
+      update => {
+        update
+          .transition()
+          .duration(250)
+          .attr("r", radius * 3) // increase radius size
+          .transition()
+          .duration(250)
+          .attr("r", radius) // bring it back to original size
+
+        return update
+      },
 
       // + HANDLE EXIT SELECTION
       exit => exit
+        .call(sel => sel
+          //before
+          .attr("opacity", 1)
+          .transition()
+          .duration(1000)
+          // after
+          .attr("opacity", 0)
+          .remove()
+        )
     );
 }
