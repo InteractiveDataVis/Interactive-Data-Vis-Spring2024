abe6c918d8f958d02c46e811e952cccbdac216d0
efrymire <ellie.frymire@gmail.com>
2024-03-25 09:15:41 -0700

    week 7

--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,25 +1,20 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
-
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = 20;
+  radius = 3;
+let svg;
+let xScale;
+let yScale;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selectedParty: "All" // + YOUR INITIAL FILTER SELECTION
+  selectedParty: "All" 
 };
 
 /* LOAD DATA */
 d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
-  // + SET YOUR DATA PATH
   console.log("data", raw_data);
   // save our data to application state
   state.data = raw_data;
@@ -29,44 +24,72 @@ d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
 /* INITIALIZING FUNCTION */
 // this will be run *one time* when the data finishes loading in
 function init() {
-  // + SCALES
+  console.log("init is run")
 
+  // + SCALES
+  xScale = d3.scaleLinear()
+  .domain([0, 1])
+  .range([0, width])
+
+  yScale = d3.scaleLinear()
+  .domain([0, 100])
+  .range([height, 0])
 
   // + AXES
 
-
   // + UI ELEMENT SETUP
+  const dropdown = d3.select("#dropdown")
+  
+  const options = dropdown
+    .selectAll("option")
+    .data(["All", "R", "D"])
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d)
 
+  dropdown
+    .on("change", (event) => {
+      // console.log(event.target.value)
+      state.selectedParty = event.target.value;
+      console.log(state)
+      draw();
+    })
 
   // + CREATE SVG ELEMENT
-
+  svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .style("overflow", "visible")
 
   // + CALL AXES
 
-
-
-  draw(); // calls the draw function
+  draw();
 }
 
 /* DRAW FUNCTION */
 // we call this every time there is an update to the data/state
 function draw() {
+  console.log("draw is run")
+
+  const filteredData = state.data.filter(d => 
+    state.selectedParty === "All" || d.Party === state.selectedParty
+  )
 
   // + FILTER DATA BASED ON STATE
-  const filteredData = state.data
-    // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
-
-  const dot = svg
-    .selectAll("circle")
+  const circles = svg.selectAll(".circle")
     .data(filteredData, d => d.BioID)
     .join(
-      // + HANDLE ENTER SELECTION
-      enter => enter,
-
-      // + HANDLE UPDATE SELECTION
-      update => update,
-
-      // + HANDLE EXIT SELECTION
-      exit => exit
-    );
+      enter => enter.append("circle")
+        .attr("cx", d => xScale(d.ideologyScore2020))
+        .attr("cy", d => yScale(d.envScore2020)),
+      update => update
+        .call(sel => sel.transition()
+          .attr("cx", d => xScale(d.ideologyScore2020))
+          .attr("cy", d => yScale(d.envScore2020))),
+      exit => exit.remove()
+    )
+    .attr("class", "circle")
+    .attr("r", radius)
+  
 }
